e6e3f8bb300c4f3c0054044604882bdc478001c5
ssh4rky <sigmovic1488@gmail.com>
2025-09-20 11:37:25 +0300

    Stack adjustment

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -1,14 +1,13 @@
 ﻿#include <iostream>
-#include <string>
 
 using namespace std;
 
 template <typename T>
-
 struct Node {
 	T value;
+	Node<T>* previous;
 	Node<T>* next;
-	Node(T val) : value(val), next(nullptr) {}
+	Node(T val) : value(val), previous(nullptr), next(nullptr) {}
 };
 
 template<typename T>
@@ -24,6 +23,9 @@ public:
 	void Append(T val);
 	void Find(T val) const;
 	void InsertAt(int index, T val);
+	void PrintList();
+
+	void forEach(void(*func)(T&));
 	T RemoveAt(int index);
 
 	int GetSize() const {
@@ -60,16 +62,129 @@ LinkedList<T>::~LinkedList() {
 	}
 }
 
+template<typename T>
+T LinkedList<T>::RemoveAt(int index) {
+	if (index < 0 || index > size) {
+		throw out_of_range("Index out of Bounds");
+	}
+	Node<T>* toDelete = nullptr;
+	if (index == 0) {
+		Node<T>* next = head->next;
+		toDelete = head;
+		head = next;
+	}
+	else {
+		Node<T>* current = head;
+		for (int i = 0; i < index - 1; i++) {
+			current = current->next;
+		}
+		toDelete = current->next;
+		current->next = toDelete->next;
+		if (toDelete == tail) {
+			tail = current;
+		}
+	}
+	size--;
+	return toDelete->value;
+	delete toDelete;
+}
+
+template <typename T>
+void LinkedList<T>::PrintList() {
+	Node<int>* current = GetHead();
+	while (current != nullptr) {
+		Node<int>* nextNode = current->next;
+		cout << current->value << " ";
+		current = nextNode;
+	}
+	cout << endl;
+}
+
+template <typename T>
+void LinkedList<T>::forEach(void(*func)(T&)) {
+	Node<T>* current = head;
+	while (current) {
+		func(current->value);
+		current = current->next;
+	}
+}
+
+void Increment(int& val) {
+	val++;
+}
+
+void Square(int& val) {
+	val *= val;
+}
+
+// Стек на основі динамічного масиву
+template<typename T>
+class Stack {
+private:
+	T* data;
+	int capacity;
+	int top;
+
+	void resize(int newCapacity) {
+		T* temp = new T[newCapacity];
+		for (int i = 0; i < capacity; i++) {
+			temp[i] = data[i];
+		}
+		delete[] data;
+		data = temp;
+		capacity = newCapacity;
+	}
+public:
+	Stack(int initCapacity = 10) : capacity(initCapacity), top(-1) {
+		data = new T[capacity];
+	}
+	~Stack() {
+		delete[] data;
+	}
+
+	bool isEmpty() const {
+		return top == -1;
+	}
+	int size() const {
+		return top + 1;
+	}
+	void clear() {
+		top = -1;
+	}
+
+	void push(const T& value) {
+		if (top + 1 >= capacity) {
+			resize(capacity * 2);
+		}
+		data[++top] = value;
+	}
+	T pop() {
+		if (isEmpty()) {
+			throw out_of_range("Stack is empty");
+		}
+		return data[top--];
+	}
+	T peek() const {
+		if (isEmpty()) {
+			throw out_of_range("Stack is empty");
+		}
+		return data[top];
+	}
+};
+
+
 int main()
 {
 	LinkedList<int> list;
 	list.Append(10);
 	list.Append(12);
+	list.Append(14);
+	list.Append(16);
+	list.PrintList();
 
-	Node<int>* current = list.GetHead();
-	while (current != nullptr) {
-		Node<int>* nextNode = current->next;
-		cout << current->value << endl;
-		current = nextNode;
-	}
+	list.forEach(Increment);
+	list.PrintList();
+
+	list.forEach(Square);
+	list.PrintList();
 }
