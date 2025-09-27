cd9ab6ef244e651ee2268f8fbe088edd75c22836
ssh4rky <sigmovic1488@gmail.com>
2025-09-27 09:28:49 +0300

    Clear

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -2,33 +2,6 @@
 
 using namespace std;
 
-template<typename T>
-class Queue {
-private:
-    T* data;
-    int capacity;
-    int size;
-    int front;
-    int last;
-
-    void reasize(int newCapacity) {
-        T* temp = new T[newCapacity];
-        for (int i = 0; i < capacity; i++) {
-            temp[i] = data[i];
-        }
-        delete[] data;
-        data = temp;
-    }
-public:
-    Queue(int initCapacity = 10) :
-        capacity(initCapacity), size(0), front(0), last(-1) {
-    }
-    ~Queue() {
-        delete[] data;
-    }
-
-};
-
 int main()
 {
 
