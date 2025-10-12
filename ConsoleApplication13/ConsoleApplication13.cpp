35c2320e7f982431f5a7f4c57fe1f1dbcf6e4a6f
ssh4rky <sigmovic1488@gmail.com>
2025-10-12 14:54:43 +0300

    Clear

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -1,53 +1,9 @@
 ﻿#include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
-class IDamageable {
-public:
-    virtual void ReceiveDamage(float damage) = 0;
-    virtual float getHealth() const = 0;
-};
-
-class Character : public IDamageable {
-private:
-    float health;
-    float damage;
-public:
-    Character(float h, float d) : health(h), damage(d) {}
-    void Attack(IDamageable& target) {
-        target.ReceiveDamage(damage);
-    }
-    void ReceiveDamage(float damage) {
-        health -= damage;
-    }
-    float getHealth() const {
-        return health;
-    }
-};
-
-class Enemy : public Character {
-public:
-    Enemy(float h, float d) : Character(h, d) {}
-};
-
-class Player : public Character {
-public:
-    Player(float h, float d) : Character(h, d) {}
-};
-
-class Barricade : public IDamageable {
-private:
-    float health;
-public:
-    Barricade(float h) : health(h) {}
-    void ReceiveDamage(float damage) {
-        health -= damage;
-    }
-    float getHealth() const {
-        return health;
-    }
-};
-
-int main() {
+int main()
+{
     
 }
