8b252510aacb6ecb84df68ae420840798bae1380
ssh4rky <sigmovic1488@gmail.com>
2025-10-19 09:32:43 +0300

    ISO C++20 standard

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -4,80 +4,12 @@
 #include <cmath>
 #include <vector>
 #include <fstream>
+#include <memory>
 
 using namespace std;
 
-class Matrix {
-private:
-	int width, length;
-	vector<vector<int>> matrix;
-public:
-	Matrix(int a, int b) : length(a), width(b) {
-		srand(time(NULL));
-		matrix = vector<vector<int>>(length, vector<int>(width));
-		for (int i = 0; i < length; i++) {
-			for (int j = 0; j < width; j++) {
-				matrix[i][j] = rand() % 10;
-			}
-		}
-	}
-
-	friend ostream& operator<<(ostream& os, const Matrix& m) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m.width; j++) {
-				os << m.matrix[i][j] << " ";
-			}
-			os << "\n";
-		}
-		return os;
-	}
-
-	friend istream& operator>>(istream& is, Matrix& m) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m.width; j++) {
-				is >> m.matrix[i][j];
-			}
-		}
-		return is;
-	}
-
-	friend ofstream& operator<<(ofstream& os, const Matrix& m) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m.width; j++) {
-				os << m.matrix[i][j] << " ";
-			}
-			os << "\n";
-		}
-		return os;
-	}
-
-	friend ifstream& operator>>(ifstream& is, Matrix& m) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m.width; j++) {
-				is >> m.matrix[i][j];
-			}
-		}
-		return is;
-	}
-};
-
-
 int main() {
-	int a, b;
-	cout << "Enter length: ";
-	cin >> a;
-	cout << "Enter width: ";
-	cin >> b;
-
-	Matrix m(a, b);
-	ofstream fout("test.txt", ios_base::app);
-	if (!fout) {
-		cerr << "Error: could not open file for writing.\n";
-		return 1;
-	}
-	fout << m;
-	fout.close();
-
-	cout << "\nGenerated matrix:\n";
-	cout << m;
+	unique_ptr<int> up1(new int(10));
+	cout << "Adress: " << up1.get() << endl;
+	cout << "Value: " << *up1 << endl;
 }
