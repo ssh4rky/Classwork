0fc80b4adab9496186d38d33c3e261d76df2747b
ssh4rky <sigmovic1488@gmail.com>
2025-10-19 12:07:21 +0300

    Train

--- ConsoleApplication13/ConsoleApplication13.cpp
+++ ConsoleApplication13/ConsoleApplication13.cpp
@@ -5,46 +5,137 @@
 #include <vector>
 #include <fstream>
 #include <memory>
+#include <cstdlib>
 
 using namespace std;
 
+struct Time {
+    int hours, minutes;
+};
+
+ostream& operator<<(ostream& os, const Time& time) {
+    os << (time.hours < 10 ? "0" : "") << time.hours << ":"
+        << (time.minutes < 10 ? "0" : "") << time.minutes;
+    return os;
+}
+
+istream& operator>>(istream& is, Time& time) {
+    char ch;
+    is >> time.hours >> ch >> time.minutes;
+    return is;
+}
+
+Time GenerateTime() {
+    Time time;
+    time.hours = rand() % 24;
+    time.minutes = rand() % 60;
+    return time;
+}
+
+class Train {
+private:
+    static int globalNumber;
+    int number;
+    string arrivalStation;
+    Time time;
+public:
+    Train() : number(++globalNumber), arrivalStation("Station"), time(GenerateTime()) {}
+
+    void SetTrainInfo() {
+        cout << "Enter time (HH:MM): ";
+        cin >> time;
+        cout << "Enter arrival station: ";
+        cin >> arrivalStation;
+    }
+
+    void GetTrainInfo() const {
+        cout << "Train number: " << number
+            << ", Time: " << time
+            << ", Station: " << arrivalStation << endl;
+    }
+
+    const string& GetStation() const { return arrivalStation; }
+    int GetNumber() const { return number; }
+};
+
+int Train::globalNumber = 0;
+
+class Station {
+private:
+    string name;
+    vector<Train> trains;
+public:
+    Station() : name("Universitatea") {}
+
+    void AddTrain() {
+        Train t;
+        t.SetTrainInfo();
+        trains.push_back(t);
+    }
+
+    void ShowAllTrains() const {
+        if (trains.empty()) {
+            cout << "No trains available.\n";
+            return;
+        }
+        for (const auto& t : trains) {
+            t.GetTrainInfo();
+        }
+    }
+
+    void ShowTrainByNumber(int num) const {
+        for (const auto& t : trains) {
+            if (t.GetNumber() == num) {
+                t.GetTrainInfo();
+                return;
+            }
+        }
+        cout << "Train with number " << num << " not found.\n";
+    }
+
+    void ShowTrainsByStation(const string& stationName) const {
+        bool found = false;
+        for (const auto& t : trains) {
+            if (t.GetStation() == stationName) {
+                t.GetTrainInfo();
+                found = true;
+            }
+        }
+        if (!found)
+            cout << "No trains to " << stationName << " found.\n";
+    }
+};
+
 int main() {
-	/*unique_ptr<int> up1(new int(10)), up2(new int(15)), up3(new int(20));
-	cout << "Adress 1: " << up1.get() << endl;
-	cout << "Adress 2: " << up2.get() << endl;
-	cout << "Adress 3: " << up3.get() << endl;
-	cout << "Value 1: " << *up1 << endl;
-	cout << "Value 2: " << *up2 << endl;
-	cout << "Value 3: " << *up3 << endl;
-	up3.release();
-	up3 = make_unique<int>(111);
-	up1.swap(up2);
-	cout << "Adress 1: " << up1.get() << endl;
-	cout << "Adress 2: " << up2.get() << endl;
-	cout << "Adress 2: " << up3.get() << endl;
-	cout << "Value 1: " << *up1 << endl;
-	cout << "Value 2: " << *up2 << endl;
-	cout << "Value 2: " << *up3 << endl;
-	cout << "(up1 <=> up2) == 0 : " << ((up1 <=> up2) == 0) << '\n';*/
+    srand(time(NULL));
+    Station st;
 
-	/*shared_ptr<double> p1(new double(45.1));*/
-	/*shared_ptr<int> p2 = p1;
-	shared_ptr<int> p3 = p2;
-	cout << "Adress 1: " << p1.get() << endl;
-	cout << "Adress 2: " << p2.get() << endl;
-	cout << "Adress 3: " << p3.get() << endl;
-	cout << "Value 1: " << *p1 << endl;
-	cout << "Value 2: " << *p2 << endl;
-	cout << "Value 3: " << *p3 << endl;*/
+    int choice;
+    do {
+        cout << "\n1) Add train\n2) Show all trains\n3) Show train by number\n4) Show trains by station\n0) Exit\nChoice: ";
+        cin >> choice;
 
-	/*weak_ptr<double> wp1 = p1;
-	if (shared_ptr<double> locked = wp1.lock()) {
-		cout << *locked << endl;
-		cout << locked.use_count() << endl;
-	}
-	else {
-		cout << "The object have already been deleted" << endl;
-	}*/
+        switch (choice) {
+        case 1: st.AddTrain(); break;
+        case 2: st.ShowAllTrains(); break;
+        case 3: {
+            int num;
+            cout << "Enter train number: ";
+            cin >> num;
+            st.ShowTrainByNumber(num);
+            break;
+        }
+        case 4: {
+            string station;
+            cout << "Enter station name: ";
+            cin >> station;
+            st.ShowTrainsByStation(station);
+            break;
+        }
+        case 0: break;
+        default: cout << "Invalid choice.\n"; break;
+        }
+    } while (choice != 0);
 
-	
+    return 0;
 }
